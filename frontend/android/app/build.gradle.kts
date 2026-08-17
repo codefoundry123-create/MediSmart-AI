de9a44d5caa0e07087f8e269e9121b7e81852a1c
codefoundry123-create <codefoundry123@gmail.com>
2026-08-17 23:08:18 +0500

    Update build.gradle.kts

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.medismart_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Disable R8/minification to avoid ML Kit missing-class errors
+            isMinifyEnabled = false
+            isShrinkResources = false
+
+            // Use debug signing for the APK
             signingConfig = signingConfigs.getByName("debug")
         }
     }
